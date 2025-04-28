919f57cde7072893cb9e0502ffaaf2a781742696
ZahwaReziFY <zahwarezi416@gmail.com>
2025-04-28 19:13:38 +0700

    add void input data

--- IntroduceOOP.cpp
+++ IntroduceOOP.cpp
@@ -7,3 +7,11 @@ class persegipanjang
     float panjang;
     float lebar;
     float luas;
+
+    void inputData(){
+        cout << "input panjang : ";
+        cin >> panjang;
+        cout << "input lebar : ";
+        cin >> lebar;
+    
+    }
